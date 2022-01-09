e32bb1242849a446e2e0829a50b4ff7cc2c283c7
Harshitha Durai <hduraib@uwaterloo.ca>
2022-01-08 20:25:38 -0500

    mobile 3

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    document.getElementById("navbar").style.top = "0";
+  if (window.innerWidth < 1000) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
   }
 }
 
